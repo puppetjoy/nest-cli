df941d0b622ca6a304fdf73853bf595f6c74c384
James Lee <james@james.tl>
2024-01-29 21:38:11 -0500

    installer: Warn about hybrid MBR on Raspberry Pi 3
    Closes #28

--- lib/nest/installer/raspberrypi.rb
+++ lib/nest/installer/raspberrypi.rb
@@ -8,6 +8,15 @@ module Nest
         swap_size = platform == 'raspberrypi3' ? '1G' : '8G'
         super(**options.merge(swap_size: swap_size))
       end
+
+      def partition(disk)
+        super(disk)
+
+        if platform == 'raspberrypi3'
+          logger.warn 'Remember to setup the hybrid MBR!'
+          logger.warn 'See: https://gitlab.james.tl/nest/puppet/-/wikis/Platforms/Raspberry-Pi#hybrid-mbr'
+        end
+      end
     end
   end
 end
